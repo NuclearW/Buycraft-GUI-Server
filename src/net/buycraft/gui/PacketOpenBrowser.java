cd74689965044c58eb3339264e37b13d7083dcdf
NuclearW <incongruency@gmail.com>
2011-12-29 22:32:39 -0500

    Add read/write and put in TODO for adding to list

--- src/net/buycraft/gui/PacketOpenBrowser.java
+++ src/net/buycraft/gui/PacketOpenBrowser.java
@@ -6,22 +6,29 @@ import org.getspout.spoutapi.io.SpoutOutputStream;
 import org.getspout.spoutapi.player.SpoutPlayer;
 
 public class PacketOpenBrowser extends AddonPacket {
-	
-	public String url;
+	private String url;
 	
 	public PacketOpenBrowser(String url) {
 		this.url = url;
 	}
 
+	@Override
 	public void read(SpoutInputStream in) {
-		
+		// Do nothing with data
 	}
 
+	@Override
 	public void run(SpoutPlayer player) {
-
+		// When a user sends us a packet, we're going to do something about it.
+		
+		// TODO: Put them in a list with player.getName();
 	}
 
+	@Override
 	public void write(SpoutOutputStream out) {
-
+		if(this.url == null) {
+			this.url = "null";
+		}
+		ouputStream.writeString(this.url);
 	}
 }
